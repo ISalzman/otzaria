c425ad9aadd8a1ecde0508fb44e4bcae812efc51
DeveShlomo <sz115682@gmaol.com>
2026-03-01 03:32:26 +0200

    סידור הגדרות ומחיקת קובץ גיבוי מיותר

--- lib/settings/safer_mode/protected_settings_wrapper.dart
+++ lib/settings/safer_mode/protected_settings_wrapper.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
-import 'package:otzaria/settings/bloc/settings_repository.dart';
-import 'package:otzaria/settings/bloc/settings_bloc.dart';
-import 'package:otzaria/settings/bloc/settings_state.dart';
+import 'package:otzaria/settings/engine/settings_repository.dart';
+import 'package:otzaria/settings/engine/settings_bloc.dart';
+import 'package:otzaria/settings/engine/settings_state.dart';
 import 'package:otzaria/settings/password_verification_dialog.dart';
 
 /// Wrapper שבודק סיסמה לפני כניסה למסך מוגן
--- test/unit/mocks/mock_settings_wrapper.mocks.dart
+++ test/unit/mocks/mock_settings_wrapper.mocks.dart
@@ -7,7 +7,7 @@ import 'dart:async' as _i4;
 
 import 'package:mockito/mockito.dart' as _i1;
 import 'package:mockito/src/dummies.dart' as _i3;
-import 'package:otzaria/utils/settings_wrapper.dart' as _i2;
+import 'package:otzaria/settings/engine/settings_wrapper.dart' as _i2;
 
 // ignore_for_file: type=lint
 // ignore_for_file: avoid_redundant_argument_values
