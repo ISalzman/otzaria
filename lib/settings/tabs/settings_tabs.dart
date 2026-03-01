f7ec5e7ed531727d7759f7b9e9b33383581009f6
DeveShlomo <sz115682@gmaol.com>
2026-03-01 14:48:51 +0200

    תיקון שגיאה בסידור ההגדרות

--- lib/settings/tabs/settings_tabs.dart
+++ lib/settings/tabs/settings_tabs.dart
@@ -2,10 +2,10 @@
 export 'design_settings_tab.dart';
 export 'reading_settings_tab.dart';
 export 'library_settings_tab.dart';
-export 'package:otzaria/tools/calendar/calendar_settings_tab.dart';
-export 'gematria_settings_tab.dart';
-export 'editor_settings_tab.dart';
 export 'tools_settings_tab.dart';
-export 'package:otzaria/shortcuts/shortcuts_settings_tab.dart';
+export 'shortcuts_settings_tab.dart';
 export 'system_settings_tab.dart';
 export 'about_dev_tab.dart';
+export '../panels/calendar_settings_tab.dart';
+export '../panels/gematria_settings_tab.dart';
+export '../panels/editor_settings_tab.dart';
