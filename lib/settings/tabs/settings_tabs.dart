b0766a65fdbd270f0ddb735840ffa0a691daf99b
DeveShlomo <sz115682@gmaol.com>
2026-03-01 03:39:26 +0200

    העברת מסך כלים ומחיקת קבצי DB ישנים ושינוי שם

--- lib/settings/tabs/settings_tabs.dart
+++ lib/settings/tabs/settings_tabs.dart
@@ -7,6 +7,5 @@ export 'gematria_settings_tab.dart';
 export 'editor_settings_tab.dart';
 export 'tools_settings_tab.dart';
 export 'package:otzaria/shortcuts/shortcuts_settings_tab.dart';
-export 'backup_settings_tab.dart';
-export 'advanced_settings_tab.dart';
+export 'system_settings_tab.dart';
 export 'about_dev_tab.dart';
