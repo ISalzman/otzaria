108ac03f5f1591afe31bb782b9a0ebd5374c264a
gaon gadol <gwngdwl@gmail.com>
2026-03-11 04:33:41 +0200

    תיקון ג'מיני

--- lib/search/view/full_text_search_screen.dart
+++ lib/search/view/full_text_search_screen.dart
@@ -1,14 +1,11 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:otzaria/tabs/models/searching_tab.dart';
-import 'package:otzaria/tabs/models/tab.dart';
 import 'package:otzaria/search/view/tantivy_full_text_search.dart';
 import 'package:flutter/material.dart';
 
 class FullTextSearchScreen extends StatelessWidget {
-  final void Function(OpenedTab) openBookCallback;
   final SearchingTab tab;
-  const FullTextSearchScreen(
-      {super.key, required this.tab, required this.openBookCallback});
+  const FullTextSearchScreen({super.key, required this.tab});
   @override
   Widget build(BuildContext context) {
     return BlocProvider.value(
