a0071fc213bc60fe0f3c7a8c58bc94f1877f5ffd
y-ploni <7353755@gmail.com>
2026-03-18 10:44:55 +0200

    תיקון חבילת האנדרואיד לשם החדש otzaria

--- android/app/src/main/kotlin/com/sivan22/otzaria/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.sivan22.otzaria
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/org/otzaria/otzaria/MainActivity.kt
@@ -0,0 +1,5 @@
+package org.otzaria.otzaria
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
