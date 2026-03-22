c97aa37ca6cb511eeb18ab5cdaf2c1406e2a3f0a
y-ploni <7353755@gmail.com>
2026-03-22 16:20:36 +0200

    Revert "שלא יוצג הגרשיים ותוצאות החיפוש"
    This reverts commit ef96309385530bd88c91c0f06e6ccc7f98bbfc79.

--- lib/search/view/tantivy_search_results.dart
+++ lib/search/view/tantivy_search_results.dart
@@ -123,9 +123,6 @@ class _TantivySearchResultsState extends State<TantivySearchResults> {
               titleText = utils.replaceHolyNames(titleText);
               rawHtml = utils.replaceHolyNames(rawHtml);
             }
-            // הסרת גרשיים מהתצוגה
-            titleText = titleText.replaceAll('"', '');
-            rawHtml = rawHtml.replaceAll('"', '');
 
             // חישוב רוחב זמין לטקסט
             final availableWidth = constrains.maxWidth - 100.0;
