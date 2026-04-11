47f1145302d3f9ab716dc688bd8df184f0aacd9f
palmoni5 <ff8401948@gmail.com>
2026-04-12 02:37:13 +0300

    עיצוב עיון, PDF וחלוניות צד - דב שלמה

--- lib/text_book/view/commentary_list_base.dart
+++ lib/text_book/view/commentary_list_base.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_context_menu/flutter_context_menu.dart' as ctx;
 import 'package:otzaria/theme/app_fonts.dart';
+import 'package:otzaria/widgets/app_menu.dart';
 import 'package:otzaria/models/links.dart';
 import 'package:otzaria/tabs/models/text_tab.dart';
 import 'package:otzaria/text_book/bloc/text_book_bloc.dart';
@@ -1147,15 +1147,16 @@ class _CollapsibleCommentaryGroupState
                 ),
               ),
               builder: (context, selectedText, child) {
-                return ctx.ContextMenuRegion(
-                  contextMenu: ContextMenuUtils.buildCommentaryContextMenu(
-                    context: context,
+                return AppContextMenuRegion(
+                  menuBuilder: (menuCtx) =>
+                      ContextMenuUtils.buildCommentaryContextMenu(
+                    context: menuCtx,
                     link: link,
                     openBookCallback: widget.openBookCallback,
                     fontSize: widget.fontSize,
                     savedSelectedText: selectedText,
                     onCopySelected: () => ContextMenuUtils.copyFormattedText(
-                      context: context,
+                      context: menuCtx,
                       savedSelectedText: selectedText,
                       fontSize: widget.fontSize,
                     ),
