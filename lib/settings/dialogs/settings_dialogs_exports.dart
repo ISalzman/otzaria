2dd9804705108cb2c422d9dbfa8f5d65d3f4abc2
DeveShlomo <sz115682@gmaol.com>
2026-04-21 02:31:09 +0300

    מחיקת קבצי פתיחת הגדרות מכל חלון שנכנסו ישירות לקבצים

--- lib/settings/dialogs/settings_dialogs_exports.dart
+++ lib/settings/dialogs/settings_dialogs_exports.dart
@@ -1,5 +1,2 @@
 // Barrel export for all settings dialogs
-export 'calendar_settings_dialog.dart';
-export 'gematria_settings_dialog.dart';
-export 'library_settings_dialog.dart';
 export 'reading_settings_dialog.dart';
