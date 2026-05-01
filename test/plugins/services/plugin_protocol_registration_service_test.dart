ba565180075fcc0a88c04d823a7fd0156a9146ca
palmoni5 <ff8401948@gmail.com>
2026-05-01 09:24:20 +0300

    תיקון רישום פרוטוקול תוספים ב-Windows למניעת תקיעה באתחול

--- test/plugins/services/plugin_protocol_registration_service_test.dart
+++ test/plugins/services/plugin_protocol_registration_service_test.dart
@@ -3,6 +3,41 @@ import 'package:otzaria/plugins/services/plugin_protocol_registration_service.da
 
 void main() {
   group('PluginProtocolRegistrationService', () {
+    test('resolveWindowsRegistryExecutable prefers WINDIR', () {
+      final executable =
+          PluginProtocolRegistrationService.resolveWindowsRegistryExecutable({
+            'WINDIR': r'D:\Windows',
+            'SystemRoot': r'C:\Windows',
+          });
+
+      expect(executable, r'D:\Windows\System32\reg.exe');
+    });
+
+    test('buildWindowsRegistrationCommands uses executable for icon and open command', () {
+      final commands =
+          PluginProtocolRegistrationService.buildWindowsRegistrationCommands(
+            r'C:\Program Files\Otzaria\otzaria.exe',
+          );
+
+      expect(commands, hasLength(4));
+      expect(commands[2], [
+        'add',
+        r'HKCU\Software\Classes\otzaria\DefaultIcon',
+        '/ve',
+        '/d',
+        r'C:\Program Files\Otzaria\otzaria.exe',
+        '/f',
+      ]);
+      expect(commands[3], [
+        'add',
+        r'HKCU\Software\Classes\otzaria\shell\open\command',
+        '/ve',
+        '/d',
+        r'"C:\Program Files\Otzaria\otzaria.exe" "%1"',
+        '/f',
+      ]);
+    });
+
     test('buildLinuxDesktopEntry does not add leading or empty lines', () {
       final entry = PluginProtocolRegistrationService.buildLinuxDesktopEntry(
         executable: '/opt/otzaria/otzaria',
