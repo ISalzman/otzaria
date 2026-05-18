ab74ac68baaed4a3f0c7a6ff080888aab9caf399
palmoni5 <d8401948@gmail.com>
2026-05-18 15:20:38 +0300

    תיקון שורת הטאבים: רוחב אחיד לימין, ייצוב סגירה והוספת טסטי רגרסיה
    - הוספת חישוב רוחב אחיד לטאבים במצב יישור לימין עם גבולות מינימום ומקסימום
    - הוספת דחיית כיווץ אחרי סגירת טאב פנימי כדי למנוע קפיצות מיותרות ב-layout
    - שמירת התנהגות המצב הממורכז ללא שינוי מהותי
    - הוספת תמיכה ב-tabWidth בתוך ScrollableTabBarWithArrows
    - הסרת התלות ב-IntrinsicWidth במסלול ללא overflow כדי למנוע בעיות layout/tooltip
    - שינוי אייקון ההצמדה כך שייפנה רוחב אמיתי כשאינו מוצג
    - תיקון בניית tooltip של כותרות טאבים עם currentTitle
    - איחוד לוגיקת בניית כותרת הטאב כדי לצמצם כפילות ולמנוע רגרסיות
    - תיקון בעיית layout במצב יישור לימין שנחשפה בזמן כתיבת הטסטים
    - הוספת טסט widget ל-ScrollableTabBarWithArrows עבור tabWidth וללא IntrinsicWidth
    - הוספת טסטי רגרסיה ל-CustomTitleBar עבור tooltip ודחיית כיווץ אחרי סגירת טאב פנימי

---
+++ test/navigation/custom_title_bar_test.dart
@@ -0,0 +1,240 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_settings_screens/flutter_settings_screens.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:otzaria/models/books.dart';
+import 'package:otzaria/navigation/bloc/navigation_bloc.dart';
+import 'package:otzaria/navigation/bloc/navigation_event.dart';
+import 'package:otzaria/navigation/bloc/navigation_state.dart';
+import 'package:otzaria/navigation/view/custom_title_bar.dart';
+import 'package:otzaria/settings/engine/settings_bloc.dart';
+import 'package:otzaria/settings/engine/settings_event.dart';
+import 'package:otzaria/settings/engine/settings_state.dart';
+import 'package:otzaria/tabs/bloc/tabs_bloc.dart';
+import 'package:otzaria/tabs/bloc/tabs_state.dart';
+import 'package:otzaria/tabs/models/text_tab.dart';
+import 'package:otzaria/text_book/bloc/text_book_bloc.dart';
+import 'package:otzaria/text_book/bloc/text_book_event.dart';
+import 'package:otzaria/text_book/bloc/text_book_state.dart';
+import 'package:otzaria/widgets/navigation/scrollable_tab_bar.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
+import '../helpers/memory_settings_cache.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() async {
+    await Settings.init(cacheProvider: MemorySettingsCache());
+  });
+
+  testWidgets('tooltip של TextBookTab כולל את כותרת המיקום בפועל',
+      (tester) async {
+    final tab = _makeTextTab('ספר א', currentTitle: 'פרק א');
+    final tabsBloc = _TestTabsBloc(
+      TabsState(tabs: [tab], currentTabIndex: 0),
+    );
+    final navigationBloc = _TestNavigationBloc(
+      const NavigationState(currentScreen: Screen.reading),
+    );
+    final settingsBloc = _TestSettingsBloc(SettingsState.initial());
+
+    addTearDown(() async {
+      tab.dispose();
+      await tabsBloc.close();
+      await navigationBloc.close();
+      await settingsBloc.close();
+    });
+
+    await _setSurfaceSize(tester, const Size(1200, 800));
+    await _pumpTitleBar(
+      tester,
+      tabsBloc: tabsBloc,
+      navigationBloc: navigationBloc,
+      settingsBloc: settingsBloc,
+    );
+
+    expect(find.byTooltip('ספר א, פרק א'), findsOneWidget);
+    expect(find.byTooltip(r'${tab.title}, $currentTitleValue'), findsNothing);
+  });
+
+  testWidgets(
+      'סגירת טאב פנימי במצב יישור לימין דוחה את כיווץ רוחב הטאבים לשתי שניות',
+      (tester) async {
+    final first = _makeTextTab('ספר א');
+    final middle = _makeTextTab('ספר ב');
+    final last = _makeTextTab('ספר ג');
+    final tabsBloc = _TestTabsBloc(
+      TabsState(tabs: [first, middle, last], currentTabIndex: 0),
+    );
+    final navigationBloc = _TestNavigationBloc(
+      const NavigationState(currentScreen: Screen.reading),
+    );
+    final settingsBloc = _TestSettingsBloc(
+      SettingsState.initial().copyWith(alignTabsToRight: true),
+    );
+
+    addTearDown(() async {
+      first.dispose();
+      middle.dispose();
+      last.dispose();
+      await tabsBloc.close();
+      await navigationBloc.close();
+      await settingsBloc.close();
+    });
+
+    await _setSurfaceSize(tester, const Size(900, 800));
+    await _pumpTitleBar(
+      tester,
+      tabsBloc: tabsBloc,
+      navigationBloc: navigationBloc,
+      settingsBloc: settingsBloc,
+    );
+
+    final initialWidths = _tabWidthsInScrollableBar(tester);
+    expect(initialWidths, hasLength(3));
+    final initialWidth = initialWidths.first;
+
+    tabsBloc.emit(TabsState(tabs: [first, last], currentTabIndex: 0));
+    await tester.pump();
+    await tester.pump();
+
+    final immediateWidths = _tabWidthsInScrollableBar(tester);
+    expect(immediateWidths, hasLength(2));
+    expect(immediateWidths.first, closeTo(initialWidth, 0.01));
+
+    await tester.pump(const Duration(seconds: 2));
+    await tester.pump();
+    await tester.pump();
+
+    final delayedWidths = _tabWidthsInScrollableBar(tester);
+    expect(delayedWidths, hasLength(2));
+    expect(delayedWidths.first, greaterThan(initialWidth));
+  });
+}
+
+Future<void> _pumpTitleBar(
+  WidgetTester tester, {
+  required TabsBloc tabsBloc,
+  required NavigationBloc navigationBloc,
+  required SettingsBloc settingsBloc,
+}) async {
+  await tester.pumpWidget(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider<TabsBloc>.value(value: tabsBloc),
+        BlocProvider<NavigationBloc>.value(value: navigationBloc),
+        BlocProvider<SettingsBloc>.value(value: settingsBloc),
+      ],
+      child: MaterialApp(
+        home: Scaffold(
+          body: SizedBox(
+            width: 900,
+            child: CustomTitleBar(
+              onReadingSettingsPressed: () {},
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+
+  await tester.pump();
+  await tester.pump(const Duration(milliseconds: 50));
+}
+
+List<double> _tabWidthsInScrollableBar(WidgetTester tester) {
+  return tester
+      .widgetList<SizedBox>(
+        find.descendant(
+          of: find.byType(ScrollableTabBarWithArrows),
+          matching: find.byType(SizedBox),
+        ),
+      )
+      .where((box) => box.child is Listener)
+      .map((box) => box.width)
+      .whereType<double>()
+      .where((width) => width >= 72 && width <= 200)
+      .toList();
+}
+
+TextBookTab _makeTextTab(String title, {String currentTitle = ''}) {
+  final book = TextBook(title: title);
+  final bloc = _TestTextBookBloc(
+    TextBookLoaded(
+      book: book,
+      showLeftPane: false,
+      content: const ['שורה א'],
+      fontSize: 18,
+      showSplitView: false,
+      activeCommentators: const [],
+      commentatorGroups: const [],
+      availableCommentators: const [],
+      links: const [],
+      visibleLinks: const [],
+      linksByLine: const {},
+      tableOfContents: const [],
+      removeNikud: false,
+      removePunctuation: false,
+      visibleIndices: const [0],
+      selectedIndex: 0,
+      pinLeftPane: false,
+      searchText: '',
+      currentTitle: currentTitle,
+      scrollController: ItemScrollController(),
+      positionsListener: ItemPositionsListener.create(),
+    ),
+  );
+
+  final tab = TextBookTab(
+    book: book,
+    index: 0,
+    blocOverride: bloc,
+  );
+  tab.currentTitle.value = currentTitle;
+  return tab;
+}
+
+class _TestTextBookBloc extends Bloc<TextBookEvent, TextBookState>
+    implements TextBookBloc {
+  _TestTextBookBloc(super.initialState) {
+    on<TextBookEvent>((event, emit) {});
+  }
+
+  @override
+  dynamic noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+}
+
+class _TestTabsBloc extends Cubit<TabsState> implements TabsBloc {
+  _TestTabsBloc(super.initialState);
+
+  @override
+  dynamic noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+}
+
+class _TestNavigationBloc extends Cubit<NavigationState>
+    implements NavigationBloc {
+  _TestNavigationBloc(super.initialState);
+
+  @override
+  void add(NavigationEvent event) {}
+
+  @override
+  dynamic noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+}
+
+class _TestSettingsBloc extends Bloc<SettingsEvent, SettingsState>
+    implements SettingsBloc {
+  _TestSettingsBloc(super.initialState) {
+    on<SettingsEvent>((event, emit) {});
+  }
+
+  @override
+  dynamic noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+}
+
+Future<void> _setSurfaceSize(WidgetTester tester, Size size) async {
+  tester.view.physicalSize = size;
+  tester.view.devicePixelRatio = 1.0;
+  addTearDown(tester.view.resetPhysicalSize);
+  addTearDown(tester.view.resetDevicePixelRatio);
+}
