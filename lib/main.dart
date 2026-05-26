2e39aa848919d015a90e7069d80a88c34b246cde
palmoni5 <d8401948@gmail.com>
2026-05-26 08:59:11 +0300

    0.9.93

--- lib/main.dart
+++ lib/main.dart
@@ -85,7 +85,7 @@ import 'package:otzaria/plugins/view/webview_environment_holder.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 
 // Updated automatically by version update scripts - do not edit manually
-const int _latestReleasedBuildNumber = 90920;
+const int _latestReleasedBuildNumber = 90930;
 
 // Global reference to window listener for cleanup
 AppWindowListener? _appWindowListener;
