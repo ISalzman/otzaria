933ad3383c91bc6c78b2700eafd2d2919f4574ba
DeveShlomo <sz115682@gmaol.com>
2026-06-08 18:45:14 +0300

    הגדרות/ווידג'ט כרטיס: העברה לתיקיית ווידג'ט

--- lib/settings/tabs/system_settings_tab.dart
+++ lib/settings/tabs/system_settings_tab.dart
@@ -37,7 +37,7 @@ import 'package:otzaria/widgets/widgets_exports.dart';
 import 'package:otzaria/widgets/dialogs/error_report_sender_email_dialog.dart';
 import 'package:otzaria/widgets/misc/rtl_icon.dart';
 import 'package:otzaria/widgets/text/rtl_text_field.dart';
-import 'package:otzaria/settings/settings_card.dart';
+import 'package:otzaria/settings/widgets/settings_card.dart';
 import 'package:otzaria/settings/widgets/settings_action_tile.dart';
 import 'package:otzaria/theme/theme_exports.dart';
 import 'package:otzaria/text_book/view/error_report_dialog.dart';
